a0728e5527dd3298f618bb38798a22ce640b16f9
Lisa T <llmt2007@aol.com>
2022-10-22 11:37:57 -0400

    Added button click event for random image

--- js/script.js
+++ js/script.js
@@ -5,7 +5,32 @@ const img = document.querySelector(".img");
 const getImage = async function(){
 	const res = await fetch("https://picsum.photos/v2/list?limit=100.");
 	const images = await res.json();
-	console.log(images);
+	//console.log(images);
+	selectRandomImage(images);
 }
 
-getImage();
+
+
+//Select the Index of an Image
+
+const selectRandomImage = function(imagePass){
+	const randomIndex = Math.floor(Math.random() * imagePass.length);
+	//console.log(randomIndex)
+	const randomImage = imagePass[randomIndex]; //dont know whats happening here
+	//console.log(randomImage);
+	displayImage(randomImage);
+}
+
+//Display the image
+
+const displayImage = function(randomImag){
+	const author = randomImag.author; 
+	const imageAddress = randomImag.download_url;
+	authorSpan.innerText = author;
+	img.src = imageAddress;
+	imgDiv.classList.remove("hide");
+}
+
+button.addEventListener("click", function(){
+	getImage();				
+						});
